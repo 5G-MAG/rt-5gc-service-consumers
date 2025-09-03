a01df5946e26f520c211f99f1077c39661322192
David Waring <david.waring2@bbc.co.uk>
2025-09-03 17:32:31 +0100

    Add unit tests for Nmbsmf_TMGI API builders

--- tests/lib/mb-smf-service-consumer/nmbsmf-builders.c
+++ tests/lib/mb-smf-service-consumer/nmbsmf-builders.c
@@ -19,6 +19,7 @@
 #include "priv_mbs-session.h"
 #include "priv_mbs-status-subscription.h"
 #include "nmbsmf-mbs-session-build.h"
+#include "nmbsmf-tmgi-build.h"
 
 #include "unit-test.h"
 
@@ -32,6 +33,33 @@ const ogs_sockaddr_t *_context_get_notification_address()
 #endif
     static const struct ogs_sockaddr_s loopback = {.sin = {.sin_family = AF_INET, .sin_addr = {lo}, .sin_port = 0}};
     return &loopback;
+}
+
+/* fake tmgi.c functions */
+
+void _tmgi_free(_priv_tmgi_t *tmgi)
+{
+    if (tmgi) {
+        if (tmgi->tmgi.mbs_service_id) ogs_free(tmgi->tmgi.mbs_service_id);
+        if (tmgi->cache) {
+            if (tmgi->cache->repr) ogs_free(tmgi->cache->repr);
+            ogs_free(tmgi->cache);
+        }
+        ogs_free(tmgi);
+    }
+}
+
+OpenAPI_tmgi_t *_tmgi_to_openapi_type(const _priv_tmgi_t *tmgi)
+{
+    if (!tmgi) return NULL;
+
+    char *mbs_service_id = NULL;
+    if (tmgi->tmgi.mbs_service_id) mbs_service_id = ogs_strdup(tmgi->tmgi.mbs_service_id);
+
+    OpenAPI_plmn_id_t *plmn = OpenAPI_plmn_id_create(ogs_plmn_id_mcc_string(&tmgi->tmgi.plmn),
+                                                     ogs_plmn_id_mnc_string(&tmgi->tmgi.plmn));
+
+    return OpenAPI_tmgi_create(mbs_service_id, plmn);
 }
 
 /* fake mbs-session.c functions */
@@ -117,6 +145,179 @@ static OpenAPI_ip_addr_t *__new_OpenAPI_ip_addr_from_in6addr(const struct in6_ad
 }
 
 /**** Tests ****/
+
+static bool test_create_tmgi(unit_test_ctx *ctx)
+{
+    _priv_tmgi_t *tmgi = (_priv_tmgi_t*)ogs_calloc(1, sizeof(*tmgi));
+    ogs_list_t new_list = {};
+
+    tmgi->cache = ogs_calloc(1, sizeof(*tmgi->cache));
+
+    ogs_list_add(&new_list, tmgi);
+    ogs_sbi_request_t *req = _nmbsmf_tmgi_build_create(&new_list, NULL);
+
+    UT_STR_EQUAL(req->h.method, OGS_SBI_HTTP_METHOD_POST);
+    if (!req->h.uri) {
+        UT_STR_EQUAL(req->h.service.name, OGS_SBI_SERVICE_NAME_NMBSMF_TMGI);
+        UT_STR_EQUAL(req->h.api.version, OGS_SBI_API_V1);
+        UT_STR_EQUAL(req->h.resource.component[0], OGS_SBI_RESOURCE_NAME_TMGI);
+        UT_STR_NULL(req->h.resource.component[1]);
+    } else {
+        UT_STR_MATCHES(req->h.uri, "^https?://(?:[a-zA-Z0-9.]*|\\[[0-9A-Fa-f:]*\\])(?::[1-9][0-9]*)?/" OGS_SBI_SERVICE_NAME_NMBSMF_TMGI "/" OGS_SBI_API_V1 "/" OGS_SBI_RESOURCE_NAME_TMGI "$");
+    }
+    cJSON *json = cJSON_Parse(req->http.content);
+    if (!json) {
+        fprintf(stderr, "expected req->http.content to be valid JSON, could not parse string as JSON: %s\n", req->http.content);
+        return false;
+    }
+    OpenAPI_tmgi_allocate_t *tmgi_alloc = OpenAPI_tmgi_allocate_parseFromJSON(json);
+    cJSON_Delete(json);
+    if (!tmgi_alloc) {
+        fprintf(stderr, "expected req->http.content to be valid 3GPP TmgiAllocate JSON, could not parse JSON as a TmgiAllocate object: %s\n", req->http.content);
+        return false;
+    }
+
+    UT_INT_EQUAL(tmgi_alloc->tmgi_number, 1);
+    UT_PTR_NULL(tmgi_alloc->tmgi_list);
+
+    OpenAPI_tmgi_allocate_free(tmgi_alloc);
+
+    _tmgi_free(tmgi);
+
+    ogs_sbi_request_free(req);
+
+    return true;
+}
+
+static bool test_refresh_tmgi(unit_test_ctx *ctx)
+{
+    _priv_tmgi_t *tmgi = (_priv_tmgi_t*)ogs_calloc(1, sizeof(*tmgi));
+    ogs_list_t refresh_list = {};
+    OpenAPI_tmgi_allocate_t *tmgi_alloc = NULL;
+    cJSON *json = NULL;
+    ogs_sbi_request_t *req = NULL;
+    bool result = false;
+    time_t now = time(NULL);
+
+#define TEST_TMGI_MBS_SERVICE_ID "123456"
+#define TEST_TMGI_PLMN_MCC 999
+#define TEST_TMGI_PLMN_MNC 01
+#define _STR(A) #A
+#define STR(A) _STR(A)
+
+    tmgi->cache = ogs_calloc(1, sizeof(*tmgi->cache));
+    tmgi->tmgi.mbs_service_id = ogs_strdup(TEST_TMGI_MBS_SERVICE_ID);
+    ogs_plmn_id_build(&tmgi->tmgi.plmn, TEST_TMGI_PLMN_MCC, TEST_TMGI_PLMN_MNC, TEST_TMGI_PLMN_MNC<100?2:3);
+    tmgi->tmgi.expiry_time = now;
+
+    ogs_list_add(&refresh_list, tmgi);
+    req = _nmbsmf_tmgi_build_create(NULL, &refresh_list);
+
+    UT_STR_EQUAL_GOTO(req->h.method, OGS_SBI_HTTP_METHOD_POST, end_test_refresh_tmgi);
+    if (!req->h.uri) {
+        UT_STR_EQUAL_GOTO(req->h.service.name, OGS_SBI_SERVICE_NAME_NMBSMF_TMGI, end_test_refresh_tmgi);
+        UT_STR_EQUAL_GOTO(req->h.api.version, OGS_SBI_API_V1, end_test_refresh_tmgi);
+        UT_STR_EQUAL_GOTO(req->h.resource.component[0], OGS_SBI_RESOURCE_NAME_TMGI, end_test_refresh_tmgi);
+        UT_STR_NULL_GOTO(req->h.resource.component[1], end_test_refresh_tmgi);
+    } else {
+        UT_STR_MATCHES_GOTO(req->h.uri, "^https?://(?:[a-zA-Z0-9.]*|\\[[0-9A-Fa-f:]*\\])(?::[1-9][0-9]*)?/" OGS_SBI_SERVICE_NAME_NMBSMF_TMGI "/" OGS_SBI_API_V1 "/" OGS_SBI_RESOURCE_NAME_TMGI "$", end_test_refresh_tmgi);
+    }
+    json = cJSON_Parse(req->http.content);
+    if (!json) {
+        fprintf(stderr, "expected req->http.content to be valid JSON, could not parse string as JSON: %s\n", req->http.content);
+        goto end_test_refresh_tmgi;
+    }
+    tmgi_alloc = OpenAPI_tmgi_allocate_parseFromJSON(json);
+    if (!tmgi_alloc) {
+        fprintf(stderr, "expected req->http.content to be valid 3GPP TmgiAllocate JSON, could not parse JSON as a TmgiAllocate object: %s\n", req->http.content);
+        goto end_test_refresh_tmgi;
+    }
+
+    UT_INT_EQUAL_GOTO(tmgi_alloc->tmgi_number, 0, end_test_refresh_tmgi);
+    UT_PTR_NOT_NULL_GOTO(tmgi_alloc->tmgi_list, end_test_refresh_tmgi);
+    UT_JSON_LIST_SIZE_GOTO(tmgi_alloc->tmgi_list, 1, end_test_refresh_tmgi);
+    OpenAPI_lnode_t *node;
+    OpenAPI_list_for_each(tmgi_alloc->tmgi_list, node) {
+        OpenAPI_tmgi_t *api_tmgi = (OpenAPI_tmgi_t*)node->data;
+        UT_PTR_NOT_NULL_GOTO(api_tmgi, end_test_refresh_tmgi);
+        UT_STR_EQUAL_GOTO(api_tmgi->mbs_service_id, TEST_TMGI_MBS_SERVICE_ID, end_test_refresh_tmgi);
+        UT_PTR_NOT_NULL_GOTO(api_tmgi->plmn_id, end_test_refresh_tmgi);
+        UT_STR_EQUAL_GOTO(api_tmgi->plmn_id->mcc, STR(TEST_TMGI_PLMN_MCC), end_test_refresh_tmgi);
+        UT_STR_EQUAL_GOTO(api_tmgi->plmn_id->mnc, STR(TEST_TMGI_PLMN_MNC), end_test_refresh_tmgi);
+    }
+
+    result = true;
+
+end_test_refresh_tmgi:
+    if (json) cJSON_Delete(json);
+    if (tmgi_alloc) OpenAPI_tmgi_allocate_free(tmgi_alloc);
+    _tmgi_free(tmgi);
+    if (req) ogs_sbi_request_free(req);
+
+    return result;
+}
+
+static bool test_delete_tmgi(unit_test_ctx *ctx)
+{
+    _priv_tmgi_t *tmgi = (_priv_tmgi_t*)ogs_calloc(1, sizeof(*tmgi));
+    ogs_list_t delete_list = {};
+    OpenAPI_tmgi_allocate_t *tmgi_alloc = NULL;
+    cJSON *json = NULL;
+    ogs_sbi_request_t *req = NULL;
+    bool result = false;
+    time_t now = time(NULL);
+    OpenAPI_tmgi_t *api_tmgi = NULL;
+
+    tmgi->cache = ogs_calloc(1, sizeof(*tmgi->cache));
+    tmgi->tmgi.mbs_service_id = ogs_strdup(TEST_TMGI_MBS_SERVICE_ID);
+    ogs_plmn_id_build(&tmgi->tmgi.plmn, TEST_TMGI_PLMN_MCC, TEST_TMGI_PLMN_MNC, TEST_TMGI_PLMN_MNC<100?2:3);
+    tmgi->tmgi.expiry_time = now;
+
+    ogs_list_add(&delete_list, tmgi);
+    req = _nmbsmf_tmgi_build_remove(&delete_list, NULL);
+
+    UT_STR_EQUAL_GOTO(req->h.method, OGS_SBI_HTTP_METHOD_DELETE, end_test_delete_tmgi);
+    if (!req->h.uri) {
+        UT_STR_EQUAL_GOTO(req->h.service.name, OGS_SBI_SERVICE_NAME_NMBSMF_TMGI, end_test_delete_tmgi);
+        UT_STR_EQUAL_GOTO(req->h.api.version, OGS_SBI_API_V1, end_test_delete_tmgi);
+        UT_STR_EQUAL_GOTO(req->h.resource.component[0], OGS_SBI_RESOURCE_NAME_TMGI, end_test_delete_tmgi);
+        UT_STR_NULL_GOTO(req->h.resource.component[1], end_test_delete_tmgi);
+    } else {
+        UT_STR_MATCHES_GOTO(req->h.uri, "^https?://(?:[a-zA-Z0-9.]*|\\[[0-9A-Fa-f:]*\\])(?::[1-9][0-9]*)?/" OGS_SBI_SERVICE_NAME_NMBSMF_TMGI "/" OGS_SBI_API_V1 "/" OGS_SBI_RESOURCE_NAME_TMGI "?tmgi-list=.*$", end_test_delete_tmgi);
+    }
+
+    /* find TMGI list in the query parameters */
+    UT_PTR_NOT_NULL_GOTO(req->http.params, end_test_delete_tmgi);
+    const char *tmgi_list_param = ogs_hash_get(req->http.params, OGS_SBI_PARAM_TMGI_LIST, OGS_HASH_KEY_STRING);
+    UT_STR_NOT_NULL_GOTO(tmgi_list_param, end_test_delete_tmgi);
+    json = cJSON_Parse(tmgi_list_param);
+    UT_PTR_NOT_NULL_GOTO(json, end_test_delete_tmgi);
+    if (!cJSON_IsArray(json)) {
+        fprintf(stderr, "expected tmgi-list query parameter to be an array\n");
+        goto end_test_delete_tmgi;
+    }
+    if (cJSON_GetArraySize(json) != 1) {
+        fprintf(stderr, "expected tmgi-list query parameter to contain one TMGI\n");
+        goto end_test_delete_tmgi;
+    }
+    api_tmgi = OpenAPI_tmgi_parseFromJSON(cJSON_GetArrayItem(json, 0));
+    UT_PTR_NOT_NULL_GOTO(api_tmgi, end_test_delete_tmgi);
+    UT_STR_EQUAL_GOTO(api_tmgi->mbs_service_id, TEST_TMGI_MBS_SERVICE_ID, end_test_delete_tmgi);
+    UT_PTR_NOT_NULL_GOTO(api_tmgi->plmn_id, end_test_delete_tmgi);
+    UT_STR_EQUAL_GOTO(api_tmgi->plmn_id->mcc, STR(TEST_TMGI_PLMN_MCC), end_test_delete_tmgi);
+    UT_STR_EQUAL_GOTO(api_tmgi->plmn_id->mnc, STR(TEST_TMGI_PLMN_MNC), end_test_delete_tmgi);
+
+    result = true;
+
+end_test_delete_tmgi:
+    if (api_tmgi) OpenAPI_tmgi_free(api_tmgi);
+    if (json) cJSON_Delete(json);
+    if (tmgi_alloc) OpenAPI_tmgi_allocate_free(tmgi_alloc);
+    _tmgi_free(tmgi);
+    if (req) ogs_sbi_request_free(req);
+
+    return result;
+}
 
 static bool test_create_sess(unit_test_ctx *ctx)
 {
@@ -392,6 +593,21 @@ static bool test_delete_status_subsc(unit_test_ctx *ctx)
 
 /** Test descriptors **/
 
+static const unit_test_t test_create_tmgi_desc = {
+    .name = "nmbsmf-tmgi: Allocate TMGI (allocate new)",
+    .fn = test_create_tmgi
+};
+
+static const unit_test_t test_refresh_tmgi_desc = {
+    .name = "nmbsmf-tmgi: Allocate TMGI (refresh)",
+    .fn = test_refresh_tmgi
+};
+
+static const unit_test_t test_delete_tmgi_desc = {
+    .name = "nmbsmf-tmgi: Deallocate TMGI",
+    .fn = test_delete_tmgi
+};
+
 static const unit_test_t test_create_sess_with_subsc_desc = {
     .name = "nmbsmf-mbssession: create session with subscription builder",
     .fn = test_create_sess_with_subsc
@@ -430,6 +646,10 @@ static const unit_test_t test_delete_status_subsc_desc = {
 __attribute__ ((constructor))
 static void _init_fn()
 {
+    register_unit_test(&test_create_tmgi_desc);
+    register_unit_test(&test_refresh_tmgi_desc);
+    register_unit_test(&test_delete_tmgi_desc);
+
     register_unit_test(&test_create_sess_desc);
     register_unit_test(&test_create_sess_with_subsc_desc);
     register_unit_test(&test_update_sess_desc);
--- tests/lib/mb-smf-service-consumer/unit-test.h
+++ tests/lib/mb-smf-service-consumer/unit-test.h
@@ -34,115 +34,151 @@ extern bool register_unit_test(const unit_test_t *test);
 
 /** Pointer tests **/
 
-#define UT_PTR_NULL(A) do { \
+#define UT_PTR_NULL_ACTION(A, ACT) do { \
         if (A) { \
             fprintf(stderr, "expected " #A " to be <null>, result 0x%p\n", (A)); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_PTR_NOT_NULL(A) do { \
+#define UT_PTR_NULL_GOTO(A, EXIT_LABEL) UT_PTR_NULL_ACTION(A, goto EXIT_LABEL)
+#define UT_PTR_NULL(A)                  UT_PTR_NULL_ACTION(A, return false)
+
+#define UT_PTR_NOT_NULL_ACTION(A, ACT) do { \
         if (!(A)) { \
             fprintf(stderr, "expected " #A " to be not <null>, result <null>\n"); \
-            return false; \
+            ACT; \
         } \
     } while (0)
+
+#define UT_PTR_NOT_NULL_GOTO(A, EXIT_LABEL) UT_PTR_NOT_NULL_ACTION(A, goto EXIT_LABEL)
+#define UT_PTR_NOT_NULL(A)                  UT_PTR_NOT_NULL_ACTION(A, return false)
 
 /** Boolean tests **/
 
-#define UT_BOOL_TRUE(A) do { \
+#define UT_BOOL_TRUE_ACTION(A, ACT) do { \
         if (!(A)) { \
             fprintf(stderr, "expected " #A " to be true, but " #A " is false\n"); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_BOOL_FALSE(A) do { \
+#define UT_BOOL_TRUE_GOTO(A, EXIT_LABEL) UT_BOOL_TRUE_ACTION(A, goto EXIT_LABEL)
+#define UT_BOOL_TRUE(A) UT_BOOL_TRUE_ACTION(A, return false)
+
+#define UT_BOOL_FALSE_ACTION(A, ACT) do { \
         if (!!(A)) { \
             fprintf(stderr, "expected " #A " to be false, but " #A " is true\n"); \
-            return false; \
+            ACT; \
         } \
     } while (0)
+
+#define UT_BOOL_FALSE_GOTO(A, EXIT_LABEL) UT_BOOL_FALSE_ACTION(A, goto EXIT_LABEL)
+#define UT_BOOL_FALSE(A) UT_BOOL_FALSE_ACTION(A, return false)
 
 /** Enumeration tests **/
 
-#define UT_ENUM_EQUAL(A, B) do { \
+#define UT_ENUM_EQUAL_ACTION(A, B, ACT) do { \
         if ((A) != (B)) { \
             fprintf(stderr, "expected " #A " == " #B ", result %i != %i\n", (A), (B)); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
+#define UT_ENUM_EQUAL_GOTO(A, B, EXIT_LABEL) UT_ENUM_EQUAL_ACTION(A, B, goto EXIT_LABEL)
+#define UT_ENUM_EQUAL(A, B)                  UT_ENUM_EQUAL_ACTION(A, B, return false)
+
 /** Integer tests **/
 
-#define UT_INT_EQUAL(A, B) do { \
+#define UT_INT_EQUAL_ACTION(A, B, ACT) do { \
         int a = (A); \
         int b = (B); \
         if (a != b) { \
             fprintf(stderr, "expected " #A " == " #B ", result %i != %i\n", a, b); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_INT_NOT_EQUAL(A, B) do { \
+#define UT_INT_EQUAL_GOTO(A, B, EXIT_LABEL) UT_INT_EQUAL_ACTION(A, B, goto EXIT_LABEL)
+#define UT_INT_EQUAL(A, B)                  UT_INT_EQUAL_ACTION(A, B, return false)
+
+#define UT_INT_NOT_EQUAL_ACTION(A, B, ACT) do { \
         int a = (A); \
         int b = (B); \
         if (a == b) { \
             fprintf(stderr, "expected " #A " != " #B ", result %i == %i\n", a, b); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
+#define UT_INT_NOT_EQUAL_GOTO(A, B, EXIT_LABEL) UT_INT_NOT_EQUAL_ACTION(A, B, goto EXIT_LABEL)
+#define UT_INT_NOT_EQUAL(A, B)                  UT_INT_NOT_EQUAL_ACTION(A, B, return false)
+
 /** size_t tests **/
 
-#define UT_SIZE_T_EQUAL(A, B) do { \
+#define UT_SIZE_T_EQUAL_ACTION(A, B, ACT) do { \
         size_t a_size = (A); \
         size_t b_size = (B); \
         if (a_size != b_size) { \
             fprintf(stderr, "expected " #A " == " #B ", result %zu != %zu\n", a_size, b_size); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_SIZE_T_NOT_EQUAL(A, B) do { \
+#define UT_SIZE_T_EQUAL_GOTO(A, B, EXIT_LABEL) UT_SIZE_T_EQUAL_ACTION(A, B, goto EXIT_LABEL)
+#define UT_SIZE_T_EQUAL(A, B)                  UT_SIZE_T_EQUAL_ACTION(A, B, return false)
+
+#define UT_SIZE_T_NOT_EQUAL_ACTION(A, B, ACT) do { \
         size_t a_size = (A); \
         size_t b_size = (B); \
         if (a_size == b_size) { \
             fprintf(stderr, "expected " #A " != " #B ", result %zu == %zu\n", a_size, b_size); \
-            return false; \
+            ACT; \
         } \
     } while (0)
+
+#define UT_SIZE_T_NOT_EQUAL_GOTO(A, B, EXIT_LABEL) UT_SIZE_T_NOT_EQUAL_ACTION(A, B, goto EXIT_LABEL)
+#define UT_SIZE_T_NOT_EQUAL(A, B)                  UT_SIZE_T_NOT_EQUAL_ACTION(A, B, return false)
 
 /** String tests **/
 
-#define UT_STR_EQUAL(A, B) do { \
+#define UT_STR_EQUAL_ACTION(A, B, ACT) do { \
         if (!(A)) { \
             fprintf(stderr, "expected " #A " == " #B ", result <null> != \"%s\"\n", (B)); \
-            return false; \
+            ACT; \
         } else if (strcmp((A), (B))) { \
             fprintf(stderr, "expected " #A " == " #B ", result \"%s\" != \"%s\"\n", (A), (B)); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_STR_NULL(A) do { \
+#define UT_STR_EQUAL_GOTO(A, B, EXIT_LABEL) UT_STR_EQUAL_ACTION(A, B, goto EXIT_LABEL)
+#define UT_STR_EQUAL(A, B)                  UT_STR_EQUAL_ACTION(A, B, return false)
+
+#define UT_STR_NULL_ACTION(A, ACT) do { \
         if (A) { \
             fprintf(stderr, "expected " #A " to be <null>, result \"%s\"\n", (A)); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_STR_NOT_NULL(A) do { \
+#define UT_STR_NULL_GOTO(A, EXIT_LABEL) UT_STR_NULL_ACTION(A, goto EXIT_LABEL)
+#define UT_STR_NULL(A)                  UT_STR_NULL_ACTION(A, return false)
+
+#define UT_STR_NOT_NULL_ACTION(A, ACT) do { \
         if (!(A)) { \
             fprintf(stderr, "expected " #A " to be not <null>, result <null>\n"); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_STR_MATCHES(STR, REGEX) do { \
+#define UT_STR_NOT_NULL_GOTO(A, EXIT_LABEL) UT_STR_NOT_NULL_ACTION(A, goto EXIT_LABEL)
+#define UT_STR_NOT_NULL(A)                  UT_STR_NOT_NULL_ACTION(A, return false)
+
+#define UT_STR_MATCHES_ACTION(STR, REGEX, ACT) do { \
         if (!(STR)) { \
             fprintf(stderr, "expected " #STR " to match regex " #REGEX ", cannot match <null> string\n"); \
-            return false; \
+            ACT; \
         } \
         PCRE2_SPTR8 regex = (PCRE2_SPTR8)(REGEX); \
         PCRE2_SPTR8 str = (PCRE2_SPTR8)(STR); \
@@ -153,48 +189,60 @@ extern bool register_unit_test(const unit_test_t *test);
             PCRE2_UCHAR err_msg[1024]; \
             pcre2_get_error_message(err, err_msg, sizeof(err_msg)); \
             fprintf(stderr, "Failed to compile regex\n%s\n    /%s/\n%*s\n", err_msg, regex, (int)err_off+1, "---^"); \
-            return false; \
+            ACT; \
         } \
         if (pcre2_match(re, str, PCRE2_ZERO_TERMINATED, 0, 0, NULL, NULL) < 0) { \
             fprintf(stderr, "expected " #STR " to match regex " #REGEX ", match failed\n"); \
-            return false; \
+            ACT; \
         } \
     } while(0)
 
-#define UT_STR_BEGINS_WITH(A, B) do {\
+#define UT_STR_MATCHES_GOTO(STR, REGEX, EXIT_LABEL) UT_STR_MATCHES_ACTION(STR, REGEX, goto EXIT_LABEL)
+#define UT_STR_MATCHES(STR, REGEX)                  UT_STR_MATCHES_ACTION(STR, REGEX, return false)
+
+#define UT_STR_BEGINS_WITH_ACTION(A, B, ACT) do {\
         if (!(A)) { \
             fprintf(stderr, "expected " #A " to begin with " #B ", but " #A " is <null>\n"); \
-            return false; \
+            ACT; \
         } \
         if (strncmp((A), (B), strlen(B))) { \
             fprintf(stderr, "expected " #A " to begin with " #B ", but \"%s\" does not begin \"%s\"\n", (A), (B)); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
-#define UT_STR_ENDS_WITH(A, B) do {\
+#define UT_STR_BEGINS_WITH_GOTO(A, B, EXIT_LABEL) UT_STR_BEGINS_WITH_ACTION(A, B, goto EXIT_LABEL)
+#define UT_STR_BEGINS_WITH(A, B)                  UT_STR_BEGINS_WITH_ACTION(A, B, return false)
+
+#define UT_STR_ENDS_WITH_ACTION(A, B, ACT) do {\
         if (!(A)) { \
             fprintf(stderr, "expected " #A " to end with " #B ", but " #A " is <null>\n"); \
-            return false; \
+            ACT; \
         } \
         size_t a_len = strlen(A); \
         size_t b_len = strlen(B); \
         if (a_len < b_len || strcmp((A)+a_len-b_len, (B))) { \
             fprintf(stderr, "expected " #A " to end with " #B ", but \"%s\" does not end \"%s\"\n", (A), (B)); \
-            return false; \
+            ACT; \
         } \
     } while (0)
 
+#define UT_STR_ENDS_WITH_GOTO(A, B, EXIT_LABEL) UT_STR_ENDS_WITH_ACTION(A, B, goto EXIT_LABEL)
+#define UT_STR_ENDS_WITH(A, B)                  UT_STR_ENDS_WITH_ACTION(A, B, return false)
+
 /** JSON List tests **/
 
-#define UT_JSON_LIST_SIZE(LIST, SIZE) do { \
+#define UT_JSON_LIST_SIZE_ACTION(LIST, SIZE, ACT) do { \
         size_t list_len = (LIST)->count; \
         size_t size_len = (SIZE); \
         if (list_len != size_len) { \
             fprintf(stderr, "expected " #LIST " to have " #SIZE " entries, result %zu != %zu\n", list_len, size_len); \
-            return false; \
+            ACT; \
         } \
     } while (0)
+
+#define UT_JSON_LIST_SIZE_GOTO(LIST, SIZE, EXIT_LABEL) UT_JSON_LIST_SIZE_ACTION(LIST, SIZE, goto EXIT_LABEL)
+#define UT_JSON_LIST_SIZE(LIST, SIZE)                  UT_JSON_LIST_SIZE_ACTION(LIST, SIZE, return false)
 
 #ifdef __cplusplus
 }
